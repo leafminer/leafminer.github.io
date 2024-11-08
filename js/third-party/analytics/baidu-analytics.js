98f54fcc386a3b6a78a1ff2e8cce3ed6b27cc129
潜叶虫 <641969353@qq.com>
2024-11-08 17:05:56 +0800

    Site updated: 2024-11-08 17:05:01

--- js/third-party/analytics/baidu-analytics.js
+++ js/third-party/analytics/baidu-analytics.js
@@ -1,7 +1,7 @@
 /* global _hmt */
 
 if (!window._hmt) window._hmt = [];
-
+document.referrer = document.location.href;
 document.addEventListener('pjax:success', () => {
   _hmt.push(['_trackPageview', location.pathname]);
 });
